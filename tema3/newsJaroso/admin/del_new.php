f17b219bdc22e1e3d0b7517d3547dfea08650458
Javier GB <jjavierguillen@gmail.com>
2020-10-07 23:42:45 +0200

    Implementado borrar noticia

--- tema3/newsJaroso/admin/del_new.php
+++ tema3/newsJaroso/admin/del_new.php
@@ -3,16 +3,17 @@
     
     //Borramos la noticia recibida por GET
     if ($_GET) {
+        //Busca el elemento del array de sesión que tiene el mismo id que recibe por GET
+        //Para ello buscamos en el array de sesión por la columna 'index'
+        $key_delete = array_search($_GET['id'], array_column($_SESSION['news'], 'index'));
 
-        //Recorrer el array de noticias y borrar aquella que tenga el mismo índice
-        foreach($_SESSION['news'] as &$new) {
-            if ($new['index'] == $_GET['id']) {
-                unset($new);
-            }
-        }
+        //Eliminamos el elemento en esa posición del array
+        unset($_SESSION['news'][$key_delete]);
+
+        //El elemento queda a null pero la posición no se borra. Rehacemos el array regenerando las keys
+        $_SESSION['news'] = array_values($_SESSION['news']);
     }
-
-    //header('Location: index.php');
+    header('Location: index.php');
 
 
 
