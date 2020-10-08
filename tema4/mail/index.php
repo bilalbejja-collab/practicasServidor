16c276eaee1b10b278e6e3c4f3b1e1ac0916d35f
Javier GB <jjavierguillen@gmail.com>
2020-10-08 12:08:28 +0200

    Modificado para que vaya con Mailjet en Heroku

--- tema4/mail/index.php
+++ tema4/mail/index.php
@@ -20,27 +20,10 @@ echo "Enviando mail con PHPMailer ...";
 $mail = new PHPMailer;
 //Tell PHPMailer to use SMTP
 $mail->isSMTP();
-//Enable SMTP debugging
-// SMTP::DEBUG_OFF = off (for production use)
-// SMTP::DEBUG_CLIENT = client messages
-// SMTP::DEBUG_SERVER = client and server messages
-/* 
-$mail->SMTPOptions = array(
-        'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-        )
-    );
-
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-*/
 
 //Set the hostname of the mail server
 $mail->Host = 'in-v3.mailjet.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
+
 //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
 $mail->Port = 465;  //2525 o 587 sin ssl
 //Set the encryption mechanism to use - STARTTLS or SMTPS
@@ -48,13 +31,11 @@ $mail->SMTPSecure = 'ssl';
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = 'cec1d7911e7da53bd169831912929ab8';
+$mail->Username = getenv('MJ_APIKEY_PUBLIC');
 //Password to use for SMTP authentication
-$mail->Password = '3b29ab9c8b5bd4e94e6bcfd8e518f496';
+$mail->Password = getenv('MJ_APIKEY_PRIVATE');
 //Set who the message is to be sent from
 $mail->setFrom('admin@erasmusiesjaroso.com');
-//Set an alternative reply-to address
-//$mail->addReplyTo('replyto@example.com', 'First Last');
 //Set who the message is to be sent to
 $mail->addAddress('jjavierguillen@gmail.com', 'JJ');
 //Set the subject line
--- tema4/mail/index_mailjet.php
+++ tema4/mail/index_mailjet.php
@@ -21,8 +21,8 @@ try {
     $mail->isSMTP();                                         // Send using SMTP
     $mail->Host       = 'in-v3.mailjet.com';                 // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                // Enable SMTP authentication
-    $mail->Username   = 'cec1d7911e7da53bd169831912929ab8';  // SMTP username
-    $mail->Password   = '3b29ab9c8b5bd4e94e6bcfd8e518f496';  // SMTP password
+    $mail->Username   = getenv('MJ_APIKEY_PUBLIC');          // SMTP username
+    $mail->Password   = getenv('MJ_APIKEY_PRIVATE');  // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;      // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       =  2525;                                // TCP port to connect to
 
