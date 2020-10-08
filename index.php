bc3308d9372f3b1e1b701aceb5715af829a5ea51
Javier GB <jjavierguillen@gmail.com>
2020-10-08 09:42:16 +0200

    Fichero index para todas las prácticas

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-    echo "Hola Mundo Loco";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prácticas Servidor 2020-2021</title>
+</head>
+<body>
+    <h1>Prácticas Servidor 2020 - 2021</h1>
+
+    <h3>Tema 2</h3>
+    <a href="tema2\ProyectoPeliculas\peliculas.php">NewsJaroso</a>
+
+    <h3>Tema 3</h3>
+    <a href="tema3\newsJaroso\index.php">NewsJaroso</a>
+
+</body>
+</html>
+
