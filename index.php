3f1c0be74c610ae885c35c5600ebbee28fde6dbe
Javier GB <jjavierguillen@gmail.com>
2020-10-08 09:48:26 +0200

    Fichero index para todas las prácticas

--- index.php
+++ index.php
@@ -10,12 +10,15 @@
     <div class="container">
         <h1 class="text-primary">Prácticas Servidor 2020 - 2021</h1>
 
-        <h3 class="text-secondary">Tema 2</h3>
+        <h3 class="text-secondary mt-2">Tema 2</h3>
         <a href="tema2\ProyectoPeliculas\peliculas.php">Películas</a>
 
-        <h3 class="text-secondary">Tema 3</h3>
+        <h3 class="text-secondary mt-2">Tema 3</h3>
         <a href="tema3\newsJaroso\index.php">NewsJaroso</a>
-        </div>
+
+        <h3 class="text-secondary mt-2">Tema 4</h3>
+        <a href="tema4\mail\index.php">Mandar email</a>
+    </div>
 </body>
 </html>
 
