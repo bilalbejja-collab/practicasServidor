d1189b96a8f4a8e1e91b038c1e6a716f2e6e531d
Javier GB <jjavierguillen@gmail.com>
2020-10-09 00:52:32 +0200

    Errores en la sesión

--- tema3/newsJaroso/admin/cerrar.php
+++ tema3/newsJaroso/admin/cerrar.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
+if (!isset($_SESSION['user-agent']))
+    $_SESSION['user-agent']=$_SERVER['HTTP_USER_AGENT'];
+else {
+    if ($_SESSION['user-agent']!=$_SERVER['HTTP_USER_AGENT'])
+        session_destroy();
+}
 
     unset($_SESSION['usuario']);
+    session_destroy();
     header("Location: index.php");
 
 ?>
