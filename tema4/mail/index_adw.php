876769802837f2402f7bddee82988b7da74b7364
Javier GB <jjavierguillen@gmail.com>
2020-10-20 22:34:52 +0200

    Cambio email

--- tema4/mail/index_adw.php
+++ tema4/mail/index_adw.php
@@ -19,9 +19,9 @@ $mail->isSMTP();
 $mail->Host = 'free.mboxhosting.com';
 
 //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 465;  //2525 o 587 sin ssl
+$mail->Port = 587;  //2525 o 587 sin ssl
 //Set the encryption mechanism to use - STARTTLS or SMTPS
-$mail->SMTPSecure = 'ssl';
+//$mail->SMTPSecure = 'ssl';
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication - use full email address for gmail
